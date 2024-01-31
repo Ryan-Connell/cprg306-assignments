bb60845b736a0f3e5e28bedcbeac48f75bf4d74e
Ryan-Connell <rconnell310@gmail.com>
2024-01-30 23:21:54 -0700

    week-2
    all changes

---
+++ app/week-2/pape.js
@@ -0,0 +1,12 @@
+// pages/index.js
+import StudentInfo from '../week-2/student-info';
+
+export default function Page() {
+  return (
+    <main>
+      <h1>Shopping List</h1>
+      <StudentInfo />
+    </main>
+  );
+}
+
